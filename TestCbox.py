9826ce3437cf79d64280b8b9e2d1593b554f1000
magnus.loof@gmail.com <magnus.loof@gmail.com>
2015-01-03 17:18:13 +0100

    added test cases for cbox

--- TestCbox.py
+++ TestCbox.py
@@ -15,6 +15,16 @@ class TestCbox(unittest.TestCase):
         self.assertEqual(keylength, _cbox.keylength)
         self.assertEqual(ctext, _cbox.ctext)
 
+    def test_file_invalid(self):
+        testfile = "test_invalid"
+        keylength = 7
+        cfile = open(testfile)
+        ctext = cfile.read()
+
+        with self.assertRaises(ValueError):
+            _cbox = cbox.Cbox(keylength, cryptofile=testfile)
+
+
     def test_text(self):
         keylength = 7
         ctext = "AABBCCDDEEFF"
@@ -22,6 +32,14 @@ class TestCbox(unittest.TestCase):
         _cbox = cbox.Cbox(keylength, cryptotext=ctext)
         self.assertEqual(keylength, _cbox.keylength)
         self.assertEqual(ctext, _cbox.ctext)
+
+    def test_text_invalid(self):
+        keylength = 7
+        ctext = "AABBCCDDEEF"
+        with self.assertRaises(ValueError):
+            _cbox = cbox.Cbox(keylength, cryptotext=ctext)
+
+
         
 if __name__ == '__main__':
     unittest.main()
--- cbox.py
+++ cbox.py
@@ -6,9 +6,16 @@ class Cbox:
         
         if cryptofile is not None:
             cfile = open(cryptofile)
-            self._ctext = cfile.read()
+            ctext = cfile.read()
+            if self.sanity_check(ctext):
+                self._ctext = ctext
+            else:
+                raise ValueError("invalid ciphertext")
         if cryptotext is not None:
-            self._ctext = cryptotext
+            if self.sanity_check(cryptotext):
+                self._ctext = cryptotext
+            else:
+                raise ValueError("invalid ciphertext")
 
     @property
     def keylength(self):
@@ -18,3 +25,18 @@ class Cbox:
     def ctext(self):
         return self._ctext
             
+    def sanity_check(self, ctext):
+        #check length of ctext - should be even number and > 0
+        length = len(ctext)
+
+        if length > 0:
+            retval = True
+        else:
+            retval = False
+            
+        if (length % 2) == 0:
+            retval = retval & True
+        else:
+            retval =  retval & False
+
+        return retval
